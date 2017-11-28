7101206aca606ebfd0204f0c22add4bfebc91ebc
Xxiong <xxiong@cqu.edu.cn>
2017-11-28 16:53:32 +0800

    try to combile sysfs and configfs

--- ramufs.c
+++ ramufs.c
@@ -7,14 +7,80 @@
 #include <linux/slab.h>
 #include <linux/init.h>
 
+struct ufs_ppa_format {
+	u8	ch_off;
+	u8	ch_len;
+	u8	lun_off;
+	u8	lun_len;
+	u8	pln_off;
+	u8	pln_len;
+	u8	blk_off;
+	u8	blk_len;
+	u8	pg_off;
+	u8	pg_len;
+	u8	sect_off;
+	u8	sect_len;
+};
+
+struct ufs_geo_l2p_mlc {
+	u16	num_pairs;
+	u8	pairs[886];
+};
+
+struct ufs_geo_l2p_tbl {
+	__u8	id[8];
+	struct ufs_geo_l2p_mlc mlc;
+};
+
+struct ufs_geo_group {
+	u8	mtype;
+	u8	fmtype;
+	u8	num_ch;
+	u8	num_lun;
+	u8	num_pln;
+	u16	num_blk;
+	u16	num_pg;
+	u16	fpg_sz;
+	u16	csecs;
+	u16	sos;
+	u32	trdt;
+	u32	trdm;
+	u32	tprt;
+	u32	tprm;
+	u32	tbet;
+	u32	tbem;
+	u32	mpos;
+	u32	mccap;
+	u16	cpar;
+	struct ufs_geo_l2p_tbl l2ptbl;
+};
+
+struct ufs_geo {
+	u8	version;
+	u8	vnvmt;
+	u8	cgrps;
+	u32	cap;
+	u32	dom;
+	struct ufs_ppa_format ppaf;
+	struct ufs_geo_group  ggrp;
+}__packed;
 
 struct ramufs {
+	struct ufs_geo *geo;
+
 	struct gendisk *gd;
-	struct kobject kobj;
+	struct kobject *kobj;
+	
 	struct configfs_subsystem ramufs_subsys;
-	int wtf;
 };
-#define kobj_to_ramufs(x) container_of(x, struct ramufs, kobj)
+//#define kobj_to_ramufs(x) container_of(x, struct ramufs, kobj)
+
+struct ramufs *ufs;
+
+
+
+
+
 
 
 static int foo;
@@ -49,31 +115,9 @@ static struct attribute_group attr_group = {
 	.attrs = attrs,
 };
 
-static struct kobject *example_kobj;
-
-static int __init ramufs_init(void)
-{
-	int retval;
-
-	example_kobj = kobject_create_and_add("kobject_example", kernel_kobj);
-	if (!example_kobj)
-		return -ENOMEM;
-
-	retval = sysfs_create_group(example_kobj, &attr_group);
-	if (retval)
-		kobject_put(example_kobj);
-
-	return retval;
-}
-
-static void __exit ramufs_exit(void)
-{
-	kobject_put(example_kobj);
-}
 
 
 
-/*
 static struct config_item *ramufs_group_make_item(struct config_group *group, const char *name)
 {
 	return (struct config_item*)NULL;
@@ -118,6 +162,22 @@ static struct configfs_subsystem ramufs_subsys = {
 static int __init ramufs_init(void)
 {
 	int ret = 0;
+	kobject *kobj;
+
+	ufs = kzalloc(sizeof(struct ramufs), GFP_KERNEL);
+	if (!ufs) {
+
+	}
+
+	ufs->kobj = kobject_create_and_add("ram-ufs", kernel_kobj);
+	if (!(ufs->kobj)) {
+		
+	}
+	ret = sysfs_create_group(ufs->kobj, &attr_group);
+	if (ret) {
+		
+		kobject_put(ufs->kobj);
+	}
 
 	pr_info("RAMUFS: ramufs init\n");
 	config_group_init(&ramufs_subsys.su_group);
@@ -137,9 +197,10 @@ out:
 static void __exit ramufs_exit(void)
 {
 	pr_err("RAMUFS: ramufs exit\n");
+	kobject_put(ufs->kobj);
 	configfs_unregister_subsystem(&ramufs_subsys);
 }
-*/
+
 module_init(ramufs_init);
 module_exit(ramufs_exit);
 
